a898b79bbf2fc3977571d56a929d8f6054f43ad9
Orhun <78608829+Orhns@users.noreply.github.com>
2023-04-30 12:23:11 +0300

    p2

---
+++ p2_variables.php
@@ -0,0 +1,16 @@
+<?php 
+$name = 'Bay Kemal';
+$price = 50;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Variables</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2>Welcome <?php echo $name; ?></h2>
+    <p>The cost of your candy is $<?php echo $price; ?> per pack.</p>
+  </body>
+</html>
