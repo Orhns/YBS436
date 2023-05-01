21f9198b727b35c9af7efedf0812a672cec5d3c8
Orhun <78608829+Orhns@users.noreply.github.com>
2023-05-01 23:59:09 +0300

    p4 edit

--- p4_oddN&while.php
+++ p4_oddN&while.php
@@ -1,4 +1,14 @@
-<h3>PHP code that prints all the odd numbers from 21 to 99</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h3>PHP code that prints all the odd numbers from 21 to 99</h3>
 <?php 
 for ($count=21; $count<=99 ; $count=$count+2) 
 { 
@@ -14,3 +24,5 @@ for ($count=21; $count<=99 ; $count=$count+2)
         ++$count1;
     }
 ?>
+</body>
+</html>
