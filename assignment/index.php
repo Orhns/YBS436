7a2793161c86e2c28f84b50e23898ef3b1adfde0
Orhun <78608829+Orhns@users.noreply.github.com>
2023-05-03 18:36:23 +0300

    bookCRM_project

---
+++ assignment/index.php
@@ -0,0 +1,48 @@
+<?php include 'includes/header.php' ?>
+
+<div class="container">
+   <div class="row">  <!-- start main content row -->
+
+      <div class="col-md-2">  <!-- start left navigation rail column -->
+         <?php include 'includes/leftnav.php'; ?>
+      </div>  <!-- end left navigation rail --> 
+
+      <div class="col-md-8">  <!-- start main content column -->
+        
+         <!-- book panel  -->
+         <div class="panel panel-danger spaceabove">           
+           <div class="panel-heading"><h4>My Customers</h4></div>
+           <table class="table">
+             <tr>
+               <th>Name</th>
+               <th>Email</th>
+               <th>University</th>
+               <th>City</th>
+             </tr>
+
+           </table>
+         </div>           
+      </div>
+      
+      <div class="col-md-2">  <!-- start left navigation rail column -->
+         <div class="panel panel-info spaceabove">
+            <div class="panel-heading"><h4>Categories</h4></div>
+               <ul class="nav nav-pills nav-stacked">
+
+               </ul> 
+         </div>
+         
+         <div class="panel panel-info">
+            <div class="panel-heading"><h4>Imprints</h4></div>
+            <ul class="nav nav-pills nav-stacked">
+
+            </ul>
+         </div>         
+      </div>  <!-- end left navigation rail --> 
+
+
+      </div>  <!-- end main content column -->
+   </div>  <!-- end main content row -->
+</div>   <!-- end container -->
+
+<?php include 'includes/footer.php' ?>
